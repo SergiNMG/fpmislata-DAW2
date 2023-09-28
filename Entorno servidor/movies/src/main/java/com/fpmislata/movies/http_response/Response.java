af0d3917f335e83abba1c2377f513e6e468cd8ee
SERGIO NICOLAS MARTINEZ GOMEZ <10297697@AD.FPMISLATA.COM>
2023-09-28 12:42:32 +0200

    response added

--- Entorno servidor/movies/src/main/java/com/fpmislata/movies/http_response/Response.java
+++ Entorno servidor/movies/src/main/java/com/fpmislata/movies/http_response/Response.java
@@ -10,15 +10,15 @@ import java.util.Optional;
 public class Response {
 
     private Object data;
-    private int totalRecords;
-    private int page;
-    private int page_size;
-    private int total_pages;
-    private int next;
-    private int previous;
+    private Integer totalRecords;
+    private Integer page;
+    private Integer page_size;
+    private Integer total_pages;
+    private String next;
+    private String previous;
 
 
-    public Response(Object data, Integer totalRecords, Optional<Integer> page, int page_size) {
+    public Response(Object data, int totalRecords, Optional<Integer> page, int page_size) {
         this.data = data;
         this.totalRecords = totalRecords;
         if(page.isPresent()){
@@ -26,8 +26,17 @@ public class Response {
         }
     }
 
-    public buildPaginationMetaData(int totalRecords, int page_size, int page){
+    private void buildPaginationMetaData(int totalRecords, int page_size, int page){
         this.page = page;
         this.page_size = page_size;
+        int total_Pages = (int) (Math.ceil((double) totalRecords / page_size));
+        this.total_pages = total_pages;
+
+        if(page > 1 && total_pages > 1){
+            this.previous = "/movies?page=" + (page - 1);
+        }
+        if (page < total_pages){
+            this.next = "/movies?page=" + (page + 1);
+        }
     }
 }
