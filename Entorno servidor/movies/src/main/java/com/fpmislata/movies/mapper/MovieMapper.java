6817d01a9ecc87cee425543b46fd9b51d32337d8
SERGIO NICOLAS MARTINEZ GOMEZ <10297697@AD.FPMISLATA.COM>
2023-10-26 13:59:03 +0200

    mapper interface added

---
+++ Entorno servidor/movies/src/main/java/com/fpmislata/movies/mapper/MovieMapper.java
@@ -0,0 +1,15 @@
+package com.fpmislata.movies.mapper;
+
+import com.fpmislata.movies.controller.model.movie.MovieDetailWeb;
+import com.fpmislata.movies.controller.model.movie.MovieListWeb;
+import com.fpmislata.movies.domain.entity.Movie;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface MovieMapper {
+    MovieMapper mapper = Mappers.getMapper(MovieMapper.class);
+
+    MovieListWeb toMovieLisWeb(Movie movie);
+    MovieDetailWeb toMovieDetailWeb(Movie movie);
+}
