b2f7ebd0cfb5eef322163b617a61159c750fa2a5
SergiNMG <serginmg00@gmail.com>
2023-10-27 00:31:24 +0200

    MovieMapper implemented

--- Entorno servidor/movies/src/main/java/com/fpmislata/movies/controller/MovieController.java
+++ Entorno servidor/movies/src/main/java/com/fpmislata/movies/controller/MovieController.java
@@ -1,17 +1,22 @@
 package com.fpmislata.movies.controller;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fpmislata.movies.controller.model.movie.MovieDetailWeb;
+import com.fpmislata.movies.controller.model.movie.MovieListWeb;
 import com.fpmislata.movies.domain.entity.Movie;
 import com.fpmislata.movies.domain.service.MovieService;
 import com.fpmislata.movies.http_response.Response;
+import com.fpmislata.movies.mapper.MovieMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequestMapping("/movies")
 @RestController
@@ -19,6 +24,8 @@ public class MovieController {
 
     @Autowired
     private MovieService movieService;
+    @Autowired
+    private MovieMapper movieMapper;
     @Value("${buildPagination.defaultPageSize}")
     private Integer page_size_default;
 
@@ -32,28 +39,20 @@ public class MovieController {
         if (page_size.isEmpty()) {
             page_size = page_size_default.describeConstable();
         }
-        return new Response(movieService.getAll(page, page_size), total_records, page, page_size);
+        //return new Response(movieService.getAll(page, page_size), total_records, page, page_size);
+        List<MovieListWeb> movieListWeb = movieService.getAll(page, page_size) //creamos una lista de la clase movieListWeb con todas las películas
+                .stream() //se crea un flujo de elementos (de tipo Movie) para procesarlos funcionalmente
+                .map(movieMapper::toMovieListWeb) // para transformar cada uno de los objetos del flujo de tipo Movie a MovieListWeb
+                .collect(Collectors.toList()); // para transformar los elementos mapeados de nuevo en una lista
 
+        return new Response(movieListWeb, total_records, page, page_size);
     }
 
     @GetMapping("/{id}")
-    public Movie findById(@PathVariable("id") int id){
-        try{
-            return movieService.findById(id);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-            throw e;
-        }
+    public MovieDetailWeb findById(@PathVariable("id") int id){
+            //return movieService.findById(id);
+            Movie movieDetailWeb = movieService.findById(id);
+            return movieMapper.toMovieDetailWeb(movieDetailWeb);
     }
 
-        /*@ResponseStatus(HttpStatus.OK)
-    @GetMapping("")
-    public List<Movie> getAll() {
-        try{
-            return movieService.getAll();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-            throw e;
-        }
-    }*/
 }
--- Entorno servidor/movies/src/main/java/com/fpmislata/movies/mapper/MovieMapper.java
+++ Entorno servidor/movies/src/main/java/com/fpmislata/movies/mapper/MovieMapper.java
@@ -1,15 +1,25 @@
 package com.fpmislata.movies.mapper;
 
+import com.fpmislata.movies.controller.model.actor.ActorListWeb;
+import com.fpmislata.movies.controller.model.director.DirectorListWeb;
 import com.fpmislata.movies.controller.model.movie.MovieDetailWeb;
 import com.fpmislata.movies.controller.model.movie.MovieListWeb;
+import com.fpmislata.movies.domain.entity.Actor;
+import com.fpmislata.movies.domain.entity.Director;
 import com.fpmislata.movies.domain.entity.Movie;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
     MovieMapper mapper = Mappers.getMapper(MovieMapper.class);
 
-    MovieListWeb toMovieLisWeb(Movie movie);
+    MovieListWeb toMovieListWeb(Movie movie);
     MovieDetailWeb toMovieDetailWeb(Movie movie);
+
+    /*DirectorListWeb toDirectorListWeb(Director director);
+
+    ActorListWeb toActorsListWeb(List<Actor> actors);*/
 }
