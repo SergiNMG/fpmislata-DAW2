d224e3eb67db097ef7e0181bd856a92062c3bf02
SergiNMG <serginmg00@gmail.com>
2023-11-12 23:15:33 +0100

    Characters null in movieDetailWeb

--- Entorno servidor/movies/src/main/java/com/fpmislata/movies/persistence/repositoryImpl/MovieReposirotyImpl.java
+++ Entorno servidor/movies/src/main/java/com/fpmislata/movies/persistence/repositoryImpl/MovieReposirotyImpl.java
@@ -65,6 +65,7 @@ public class MovieReposirotyImpl implements MovieRepository {
         try(Connection connection = DBUtil.open(true)){
             MovieEntity movieEntity = movieDAO.findById(connection, id).get();
             movieEntity.getDirectorEntity(connection, directorDAO);
+            movieEntity.getCharacterMovieEntityList(connection, characterMovieDAO);
             movieEntity.getCharacterMovieEntityList(connection, characterMovieDAO).forEach(character -> character.getActorEntity(connection, actorDAO));
             return Optional.ofNullable(MovieMapper.mapper.toMovie(movieEntity));
             // return movieEntity.map(MovieMapper.mapper::toMovie);
